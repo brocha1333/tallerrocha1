c53cf3616ec9c445fb89be1dcd783ea6e6823578
brocha1333 <brocha@uach.mx>
2025-05-28 17:08:07 -0600

    Update programa1.py

--- programa1.py
+++ programa1.py
@@ -1,1 +1,19 @@
 import streamlit as st
+# Material de contenido
+  * Simulaciones 
+# Columnas con imagenes
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+with col2:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col1:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
