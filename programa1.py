5a19db448bffd7fd40640b7b9771eee52964682a
brocha1333 <brocha@uach.mx>
2025-05-28 17:29:26 -0600

    Update programa1.py

--- programa1.py
+++ programa1.py
@@ -15,3 +15,14 @@ with col2:
 with col3:
     st.header("An owl")
     st.image("https://static.streamlit.io/examples/owl.jpg")
+
+# Calendario
+import streamlit as st
+import datetime
+
+st.title("Ejemplo de fecha")
+
+# Widget: date_input
+fecha = st.date_input("Selecciona una fecha", value=datetime.date.today())
+
+st.write("La fecha seleccionada es:", fecha)
